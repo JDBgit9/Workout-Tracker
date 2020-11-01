43d0455ccf5f190e9db5f61a710b10f4ec675a71
JDBgit9 <info@jamesdbeeks.com>
2020-11-01 12:04:38 -0500

    loading the packages

--- routes/view.js
+++ routes/view.js
@@ -1,19 +1,12 @@
-// const db = require('../models')
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 
-module.exports = (app) => {
-//index
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-//exercise
-app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
-});
-//stats
-app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
-});
-}
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
+module.exports = router;
