258d4f23ab282e3c6e82335307cfdcf56f1a3fb1
JDBgit9 <jamesthejust99@outlook.com>
2022-05-21 18:06:11 -0400

    npm i

--- server.js
+++ server.js
@@ -2,29 +2,26 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-// Connection URI
-//const uri = 
-//"mongodb+srv://110120MongoDBatlas_:110120MongoDBatlas_@workout-tracker.bkgq3.mongodb.net/?retryWrites=true&w=majority"
-
-
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
 // routes
-/*app.use(require("./routes/api.js"));*/
-app.get('/', function(req, res){
-  res.render('/routes/api.js');
-});
+app.use(require("./routes/api.js"));
+/*app.get('/', function(req, res){
+  res.render('./routes/api.js');
+});*/
 
-/*app.use(require("./routes/view.js"));*/
-
+app.use(require("./routes/view.js"));
+/*app.get('/', function(req, res){
+  res.render('/routes/index.js');
+});*/
 
 
 const PORT = process.env.PORT||3000;
