3de005e8391824f19f24aad6a6468db13078e890
JDBgit9 <jamesthejust99@outlook.com>
2022-05-24 20:32:05 -0400

    updated mongoose

--- server.js
+++ server.js
@@ -7,21 +7,17 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost:27017/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
 // routes
 app.use(require("./routes/api.js"));
-/*app.get('/', function(req, res){
+
   res.render('./routes/api.js');
-});*/
 
 app.use(require("./routes/view.js"));
-/*app.get('/', function(req, res){
-  res.render('/routes/index.js');
-});*/
 
 
 const PORT = process.env.PORT||3000;
